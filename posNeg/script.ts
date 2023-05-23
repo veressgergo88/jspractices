16822d10470032355640b366b13461ea21acfb40
veressgergo88 <veress.gergo88@gmail.com>
2023-05-24 00:39:18 +0200

    continue the posNeg but finished yet

--- posNeg/script.ts
+++ posNeg/script.ts
@@ -1,4 +1,19 @@
-let x: number = 5
-let y: number = 6
-let result: number = x + y
-console.log(result)
+function posNeg(a: number, b: number, negative: boolean){
+  if (((a < 0) && (b < 0)) && (negative === true)){
+    return true
+  }
+  else if (((a < 0) || b < 0) && (negative === false)){
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+let result1: boolean = posNeg(1, -1, false)
+let result2: boolean = posNeg(-1, 1, false)
+let result3: boolean = posNeg(-4, -5, true)
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
